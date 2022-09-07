8bbcf72a137b6f1d2a5449abf2d8082defdd8986
Amkellely <108361555+Amkellely@users.noreply.github.com>
2022-09-07 15:39:14 +0300

    Раздел Boolean(Задачи и Диаграммы)

---
+++ Main.java
@@ -0,0 +1,63 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        //Козловский Константин
+	        //Boolean 17. Дано целое положительное число.
+        // Проверить истинность высказывания: «Данное число является нечетным трехзначным».
+       /* Scanner in = new Scanner(System.in);
+        int A;
+        System.out.println("Введите число A : ");
+        A=in.nextInt();
+        boolean x;
+        x=(A>99) & (A<1000) & (A%2!=0);
+        System.out.println(x); */
+                //Boolean 19. Проверить истинность высказывания: «Среди трех данных целых
+        //чисел есть хотя бы одна пара взаимно противоположных».
+        /*Scanner in = new Scanner(System.in);
+        int A,B,C;
+        System.out.println("Введите число A : ");
+        A=in.nextInt();
+        System.out.println("Введите число B : ");
+        B=in.nextInt();
+        System.out.println("Введите число C : ");
+        C=in.nextInt();
+        boolean x;
+        x=(A==(-1)*B)|(A==(-1)*C)|(B==(-1)*C);
+        System.out.println(x); */
+                 //Boolean 21. . Дано трехзначное число. Проверить истинность высказывания:
+        //«Цифры данного числа образуют возрастающую последовательность».
+        /*Scanner in = new Scanner(System.in);
+        int A;
+        System.out.println("Введите число A : ");
+        A=in.nextInt();
+        boolean x;
+        x=((A/100)<(A%100/10)) & ((A%100/10)<(A%10));
+        System.out.println(x); */
+                //Boolean 24. . Даны числа A, B, C (число A не равно 0). Рассмотрев дискрими-
+        //нант D = B
+        //2 − 4·A·C, проверить истинность высказывания: «Квадратное
+        //уравнение A·x2 + B·x + C = 0 имеет вещественные корни»
+        /*Scanner in = new Scanner(System.in);
+        double A,B,C,D
+        System.out.println("Введите число A : ");
+        A=in.nextDouble();
+        System.out.println("Введите число B : ");
+        B=in.nextDouble();
+        System.out.println("Введите число C : ");
+        C=in.nextDouble();
+        D=B*B-4*A*C;
+        boolean x;
+        x=D>=0;
+        System.out.println(x);*/
+                //Boolean 25.
+        /* Scanner in = new Scanner(System.in);
+        float x, y;
+        System.out.print("x:");
+        x = in.nextFloat();
+        System.out.print("y:");
+        y = in.nextFloat();
+        System.out.println(x < 0 & y > 0);*/
+    }
+}
