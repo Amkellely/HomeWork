01c0b91def6109ee53eac0b7b5d38801d4f3fbfb
Amkellely <108361555+Amkellely@users.noreply.github.com>
2022-10-13 21:38:08 +0300

    Lesson Collections

---
+++ Lesson22/Adder.java
@@ -0,0 +1,15 @@
+package Lesson22;
+
+public class Adder {
+    //складывает два числа
+    public static int add(int a, int b){
+        return a + b;
+    }
+    public static float add(float a, float b){
+        return a + b;
+    }
+    public static long add(long a, long b){
+        return a + b;
+    }
+
+}
