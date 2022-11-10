23378aaf977cbf68ae54a67a2c1d2a926866a42b
Amkellely <108361555+Amkellely@users.noreply.github.com>
2022-11-10 21:27:05 +0300

    практика

--- company/Main.java
+++ company/Main.java
@@ -1,29 +1,82 @@
 package com.company;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Arrays;
 import java.util.List;
-import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        final List<Integer> l1 = new ArrayList<>();
-        final List<Integer> l2 = new ArrayList<>();
-        for(int i =1; i<=5; i++){
-        l1.add(1); l1.add(2); l1.add(3); l1.add(4); l1.add(5);
-    }
-        l2.addAll(l1);
-        l1.clear();
-        System.out.println("l1: " + l1);
-        System.out.println("l2: " + l2);
-
-        //Задача 2. Вывести второй список с помощью итератора
-
-        Iterator<Integer> itr = l2.iterator();
-        while (itr.hasNext())
-        {
-            System.out.print(itr.next()+ " " );
+        List<String>list = new ArrayList<>();
+        Scanner in= new Scanner(System.in);
+	while (true){
+	    printMenu();
+        int choice;
+	    choice = in.nextInt();
+	        switch (choice){
+                case 1: break;
+                addElem();
+                case 2: break;
+                removeElem(list);
+                case 3: break;
+                containsElem(list);
+                case 4: break;
+                case 5: break;
+                case 6: break;
+                case 7: break;
+                case 8: break;
+                case 9: return;
+                default:
+                    System.out.println(" Ошибка = Нет такого пункта");
         }
     }
-}
+    }
+    static void printMenu(){
+        System.out.println("Добавить элемент в список.");
+        System.out.println("Удалить элемент из списка.");
+        System.out.println("Показать содержимое списка.");
+        System.out.println("Проверить есть ли значение в списке.");
+        System.out.println("Заменить значение в списке.");
+        System.out.println("Выход");
+        System.out.println("Введите номер нужного пункта: ");
+    }
+    static void addElem(List<String> list) {
+        if (list != null) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Ввведите строку");
+            String str = in.nextLine();
+            list.add(str);
+        } else {
+            System.out.println("Список пуст");
+        }
+    }
+    static void removeElem(List<String> list){
+        if (list != null) {
+        Scanner in= new Scanner(System.in);
+        System.out.println("Введите номер строки котору нужно удалить: ");
+        String str = in.nextLine();
+            if(str!=null);
+            System.out.println(list.remove(str));
+             }
+        }
+
+    static void printList(List<String> list){
+        if (list != null) {
+            Scanner in= new Scanner(System.in);
+            String str = in.nextLine();
+        for(String i: list){
+        System.out.println(" " +i);
+                }
+            }
+        else {
+            System.out.println("Список пуст");
+            }
+          }
+    static void containsElem(List<String> list){
+        if(list !=null);
+        System.out.println("Список пуст");
+    }else
+            System.out.println("Список пуст");
+        }
+
