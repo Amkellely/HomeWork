db1cfffc061bf9a763fa64a383c73a1aaf7d990e
Amkellely <108361555+Amkellely@users.noreply.github.com>
2022-12-13 21:11:50 +0300

    Add files via upload

--- sample/Main.java
+++ sample/Main.java
@@ -1,86 +1,92 @@
 package sample;
 
+import java.util.Random;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class Main extends Application {
-    public static final int WINDOW_WIDTH = 800;
-    public static final int WINDOW_HEIGHT = 800;
-    public static final int BALL_RADIUS = 15;
-    public static final int PADDLE_WIDTH = 25;
-    public static final int PADDLE_HEIGHT = 100;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(WINDOW_WIDTH, WINDOW_HEIGHT);
+    private static final int width = 800;
+    private static final int height = 600;
+    private static final int PLAYER_HEIGHT = 100;
+    private static final int PLAYER_WIDTH = 15;
+    private static final double BALL_R = 15;
+    private int ballYSpeed = 1;
+    private int ballXSpeed = 1;
+    private double playerOneYPos = height / 2;
+    private double playerTwoYPos = height / 2;
+    private double ballXPos = width / 2;
+    private double ballYPos = height / 2;
+    private int scoreP1 = 0;
+    private int scoreP2 = 0;
+    private boolean gameStarted;
+    private int playerOneXPos = 0;
+    private double playerTwoXPos = width - PLAYER_WIDTH;
+
+    public void start(Stage stage) throws Exception {
+        Canvas canvas = new Canvas(width, height);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        Timeline tl = new Timeline(new KeyFrame(Duration.millis(17), e-> run(gc)));
+        Timeline tl = new Timeline(new KeyFrame(Duration.millis(10), e -> run(gc)));
         tl.setCycleCount(Timeline.INDEFINITE);
-        primaryStage.setScene(new Scene(new StackPane(canvas)));
-        primaryStage.show();
+        canvas.setOnMouseMoved(e ->  playerOneYPos  = e.getY());
+        canvas.setOnMouseClicked(e ->  gameStarted = true);
+        stage.setScene(new Scene(new StackPane(canvas)));
+        stage.show();
         tl.play();
-
-        //Линия
-       /* Line line = new Line(WINDOW_WIDTH/2, 0, WINDOW_WIDTH/2, WINDOW_HEIGHT);
-        line.setStroke(Color.ORANGE);
-        line.setStrokeWidth(5);
-
-        //Мяч
-        Circle ball = new Circle(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, BALL_RADIUS);
-        ball.setFill(Color.WHITE);
-
-        //ракетки для игры
-            //левая
-        Rectangle leftPaddle = new Rectangle(PADDLE_WIDTH*1.5,PADDLE_HEIGHT);
-        leftPaddle.setX(PADDLE_WIDTH*1.5);
-        leftPaddle.setY((WINDOW_HEIGHT - PADDLE_HEIGHT)/2);
-        leftPaddle.setFill(Color.WHITE);
-            //правая
-        Rectangle rightPaddle = new Rectangle(PADDLE_WIDTH,PADDLE_HEIGHT);
-        rightPaddle.setX(WINDOW_WIDTH - 2*PADDLE_WIDTH);
-        rightPaddle.setY((WINDOW_HEIGHT - PADDLE_HEIGHT)/2);
-
-        //Создание группы
-        Group group = new Group(line, ball, leftPaddle, rightPaddle);
-        //group.getChildren().add(ball);
-
-        //Создание сцены
-        Scene scene = new Scene(group, WINDOW_WIDTH, WINDOW_HEIGHT);
-
-        //настройка цвета сцены
-        scene.setFill(Color.BROWN);
-
-        //Установка заголовка для сцены
-        primaryStage.setTitle("Sample Application");
-
-        //Добавление сцены в рабочую область
-        primaryStage.setScene(scene);
-
-        //Отображение содержимого сцены
-        primaryStage.show(); */
     }
-    private void run(GraphicsContext gc){
+
+    private void run(GraphicsContext gc) {
         gc.setFill(Color.BLACK);
-        gc.fillRect(0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
+        gc.fillRect(0, 0, width, height);
         gc.setFill(Color.WHITE);
-
-        gc.setFill(Color.WHITE);
-        gc.fillRect(WINDOW_WIDTH/2-2,0,4,WINDOW_HEIGHT);
-
-    }
-        public static void main(String args[]){
-        launch(args);
+        gc.setFont(Font.font(25));
+        if(gameStarted) {
+            ballXPos+=ballXSpeed;
+            ballYPos+=ballYSpeed;
+            if(ballXPos < width - width  / 4) {
+                playerTwoYPos = ballYPos - PLAYER_HEIGHT / 2;
+            }  else {
+                playerTwoYPos =  ballYPos > playerTwoYPos + PLAYER_HEIGHT / 2 ?playerTwoYPos += 1: playerTwoYPos - 1;
+            }
+            gc.fillOval(ballXPos, ballYPos, BALL_R, BALL_R);
+        } else {
+            gc.setStroke(Color.YELLOW);
+            gc.setTextAlign(TextAlignment.CENTER);
+            gc.strokeText("Click to Start", width / 2, height / 2);
+            ballXPos = width / 2;
+            ballYPos = height / 2;
+            ballXSpeed = new Random().nextInt(2) == 0 ? 1: -1;
+            ballYSpeed = new Random().nextInt(2) == 0 ? 1: -1;
         }
+        if(ballYPos > height || ballYPos < 0) ballYSpeed *=-1;
+        if(ballXPos < playerOneXPos - PLAYER_WIDTH) {
+            scoreP2++;
+            gameStarted = false;
+        }
+        if(ballXPos > playerTwoXPos + PLAYER_WIDTH) {
+            scoreP1++;
+            gameStarted = false;
+        }
+        if( ((ballXPos + BALL_R > playerTwoXPos) && ballYPos >= playerTwoYPos && ballYPos <= playerTwoYPos + PLAYER_HEIGHT) ||
+                ((ballXPos < playerOneXPos + PLAYER_WIDTH) && ballYPos >= playerOneYPos && ballYPos <= playerOneYPos + PLAYER_HEIGHT)) {
+            ballYSpeed += 1 * Math.signum(ballYSpeed);
+            ballXSpeed += 1 * Math.signum(ballXSpeed);
+            ballXSpeed *= -1;
+            ballYSpeed *= -1;
+        }
+        gc.fillText(scoreP1 + "\t\t\t\t\t\t\t\t" + scoreP2, width / 2, 100);
+        gc.fillRect(playerTwoXPos, playerTwoYPos, PLAYER_WIDTH, PLAYER_HEIGHT);
+        gc.fillRect(playerOneXPos, playerOneYPos, PLAYER_WIDTH, PLAYER_HEIGHT);
+    }
+
 }
